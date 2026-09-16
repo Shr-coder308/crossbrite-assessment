e055d257335cabaab22512fe65516ea631510331
Shr-coder308 <Shreyanshsoni756@gmail.com>
2026-09-16 23:00:27 +0530

    feat: add Alembic database migrations

--- app/main.py
+++ app/main.py
@@ -5,7 +5,6 @@ from app.api.evaluations import router as evaluations_router
 from app.api.parent_child import router as parent_child_router
 from app.api.sessions import router as sessions_router
 from app.api.users import router as users_router
-from app.db.database import create_tables
 from app.models import User
 
 
@@ -15,7 +14,6 @@ app = FastAPI(
     version="1.0.0",
 )
 
-create_tables()
 
 app.include_router(users_router)
 app.include_router(auth_router)
